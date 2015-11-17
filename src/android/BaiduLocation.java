6bfd7738dca34d40ce6fe3c233cb01c62d63f1b5
wwenhui <wwenhui@weforpay.com>
2015-11-17 23:22:29 +0800

    1.修复不能定位问题

--- src/android/BaiduLocation.java
+++ src/android/BaiduLocation.java
@@ -1,5 +1,4 @@
-package com.spout.phonegap.plugins.baidulocation;
-
+package com.valuecreative.phonegap.plugins.baidulocation;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,12 +10,14 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.os.Looper;
 import android.util.Log;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
+import com.baidu.location.LocationClientOption.LocationMode;
 
 public class BaiduLocation extends CordovaPlugin {
 
@@ -32,7 +33,23 @@ public class BaiduLocation extends CordovaPlugin {
 	private static final Map<Integer, String> ERROR_MESSAGE_MAP = new HashMap<Integer, String>();
 
 	private static final String DEFAULT_ERROR_MESSAGE = "服务端定位失败";
+	private static final String DEFAULT_KEY_ERROR_MESSAGE = "key验证失败";
 
+	/*	
+	    62 ： 扫描整合定位依据失败。此时定位结果无效。\n
+		63 ： 网络异常，没有成功向服务器发起请求。此时定位结果无效。\N
+		65 ： 定位缓存的结果。\N
+		66 ： 离线定位结果。通过REQUESTOFFLINELOCAITON调用时对应的返回结果\N
+		67 ： 离线定位失败。通过REQUESTOFFLINELOCAITON调用时对应的返回结果\N
+		68 ： 网络连接失败时，查找本地离线定位时对应的返回结果\N
+		161： 表示网络定位结果\N
+		162~167： 服务端定位失败\N
+		502：KEY参数错误\N
+		505：KEY不存在或者非法\N
+		601：KEY服务被开发者自己禁用\N
+		602：KEY MCODE不匹配\N
+		501～700：key验证失败\n
+	*/
 	static {
 		ERROR_MESSAGE_MAP.put(61, "GPS定位结果");
 		ERROR_MESSAGE_MAP.put(62, "扫描整合定位依据失败。此时定位结果无效");
@@ -42,12 +59,26 @@ public class BaiduLocation extends CordovaPlugin {
 		ERROR_MESSAGE_MAP.put(67, "离线定位失败。通过requestOfflineLocaiton调用时对应的返回结果");
 		ERROR_MESSAGE_MAP.put(68, "网络连接失败时，查找本地离线定位时对应的返回结果。");
 		ERROR_MESSAGE_MAP.put(161, "表示网络定位结果");
+		ERROR_MESSAGE_MAP.put(162, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(163, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(164, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(165, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(166, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(167, "服务端定位失败");
+		ERROR_MESSAGE_MAP.put(502, "key参数错误");
+		ERROR_MESSAGE_MAP.put(505, "key不存在或者非法");
+		ERROR_MESSAGE_MAP.put(601, "key服务被开发者自己禁用");
+		ERROR_MESSAGE_MAP.put(602, "key mcode不匹配");
 	};
-
+	
 	public String getErrorMessage(int locationType) {
 		String result = ERROR_MESSAGE_MAP.get(locationType);
 		if (result == null) {
-			result = DEFAULT_ERROR_MESSAGE;
+			if(locationType>500){
+				result = DEFAULT_KEY_ERROR_MESSAGE;
+			}else{
+				result = DEFAULT_ERROR_MESSAGE;
+			}
 		}
 		return result;
 	}
@@ -57,26 +88,37 @@ public class BaiduLocation extends CordovaPlugin {
 			final CallbackContext callbackContext) {
 		setCallbackContext(callbackContext);
 		if (GET_ACTION.equals(action)) {
-			cordova.getActivity().runOnUiThread(new Runnable() {
-				@Override
-				public void run() {
-					locationClient = new LocationClient(cordova.getActivity());
-					//locationClient.setAK("6O05t6mXGYFGGTOirjP8ZPFx");//设置百度的ak
-					myListener = new MyLocationListener();
-					locationClient.registerLocationListener(myListener);
-					LocationClientOption option = new LocationClientOption();
-					option.setOpenGps(true);
-					option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度，默认值gcj02
-					option.setProdName("BaiduLoc");
-					option.disableCache(true);// 禁止启用缓存定位
-					locationClient.setLocOption(option);
+				cordova.getActivity().runOnUiThread(new Runnable(){
 
-					locationClient.start();
-					locationClient.requestLocation();
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						locationClient = new LocationClient(cordova.getActivity());
+						myListener = new MyLocationListener();
+						locationClient.registerLocationListener(myListener);
+						cordova.getThreadPool().execute(new Runnable() {
+							@Override
+							public void run() {
+								LocationClientOption option = new LocationClientOption();
+								option.setOpenGps(true);
+								option.setLocationMode(LocationMode.Hight_Accuracy);// 设置定位模式
+								option.setCoorType("bd09ll");// 返回的定位结果是百度经纬度，默认值gcj02
+								option.setLocationNotify(true);//可选，默认false，设置是否当gps有效时按照1S1次频率输出GPS结果
+								option.setIgnoreKillProcess(true);//可选，默认true，定位SDK内部是一个SERVICE，并放到了独立进程，设置是否在stop的时候杀死这个进程，默认不杀死
+								option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+								option.disableCache(true);
+								locationClient.setLocOption(option);
+			
+								locationClient.start();
+								locationClient.requestLocation();
+			
+							}
 
-				}
-
-			});
+						});	
+					}
+					
+				});
+				
 			return true;
 		} else if (STOP_ACTION.equals(action)) {
 			locationClient.stop();
